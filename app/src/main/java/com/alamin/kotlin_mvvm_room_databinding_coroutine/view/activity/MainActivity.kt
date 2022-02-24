98acf256bed319615fc178f1e2adc018cdb7ad2b
Al-AminJoy <alaminislam3555@gmail.com>
2022-02-25 05:22:21 +0600

    Init

---
+++ app/src/main/java/com/alamin/kotlin_mvvm_room_databinding_coroutine/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alamin.kotlin_mvvm_room_databinding_coroutine.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alamin.kotlin_mvvm_room_databinding_coroutine.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
